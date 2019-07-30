df23411e73ad240309856c62cd5352f4ddf78e61
johannes.stroebele <johannes.stroebele@btelligent.com>
2019-07-30 10:57:21 +0200

    Setup the basic Angular app

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // Add name property of the AppComponent class
+  name = '';
+
+  title = 'Family Recipes';
+
+}
